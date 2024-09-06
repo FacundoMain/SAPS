8ff3c0afe40a187469cb1fcba7c27d737c9d68a4
facundomain <facundo.main@ingenieria.uner.edu.ar>
2024-09-06 17:45:37 -0300

    Codigos proyecto2 listo

--- Proyecto2/Implementacion_Filtros_Analogicos.py
+++ Proyecto2/Implementacion_Filtros_Analogicos.py
@@ -84,7 +84,7 @@ R2_1_c = 3.9e3
 R3_1_c = 0
 R4_1_c = np.inf
 C1_1_c = C1_1
-C2_1_c = 1.5e-8
+C2_1_c = 2.0e-9
 print('Los componentes comerciales para la sección 1 son:')
 print('R1: {:.2e} Ω'.format(R1_1_c))
 print('R2: {:.2e} Ω'.format(R2_1_c))
@@ -101,7 +101,7 @@ print('\r\n')
 # Se propone el valor de C1
 C1_2 = 100e-9 # 100nF
 
-w0_2 = np.sqrt(den_1[2])    # El termino independiente es w0^2
+w0_2 = np.sqrt(den_2[2])    # El termino independiente es w0^2
 alpha_2 = den_2[1] / w0_2   # El termino que acompaña a s es alpha*w0
 H_2 = num_2[0] / den_2[2]   # Numerador = H * w0^2
 
@@ -135,12 +135,12 @@ print('C2: {:.2e} F'.format(C2_2))
 print('\r\n')
 
 # Se utilizaran componentes con valores comerciales para la implementación
-R1_2_c = 3.9e3
-R2_2_c = 3.9e3
+R1_2_c = 150e3
+R2_2_c = 200e3
 R3_2_c = 0
 R4_2_c = np.inf
 C1_2_c = C1_1
-C2_2_c = 1.5e-8
+C2_2_c = 33e-9
 print('Los componentes comerciales para la sección 2 son:')
 print('R1: {:.2e} Ω'.format(R1_2_c))
 print('R2: {:.2e} Ω'.format(R2_2_c))
@@ -150,14 +150,18 @@ print('C1: {:.2e} F'.format(C1_2_c))
 print('C2: {:.2e} F'.format(C2_2_c))
 print('\r\n')
 
+#   Vectores con las distintas frecuencias y su ganancia del filtro implementado
+freqPrueba = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 80, 90, 100]
+amplitudPrueba = [0, 0, 0, -0.0168, -0.340, -1.85, -3.69, -14.3, -18.75, -22.6, -24.7,-28, -29.45, -31.4, -34.3,-35.8, -38.75]
+
 # %% Comparación de las respuestas en frecuncia de los filtros simulados con
 # componentes comerciales
 
 # Luego de simular los filtros utilizando el software LTSpice, se cargan los
 # resultados obtenidos para realizar la comparación con el diseño "ideal"
 
-f1, h_sim_1, _ = import_AC_LTSpice('Analisis_Frecuencial_SallenKey_PasaBajos_SEC1.txt')
-f2, h_sim_2, _ = import_AC_LTSpice('Analisis_Frecuencial_SallenKey_PasaBajos_SEC2.txt')
+f1, h_sim_1, _ = import_AC_LTSpice('Analisis_Frecuencial_SallenKey_SECCION1.txt')
+f2, h_sim_2, _ = import_AC_LTSpice('Analisis_Frecuencial_SallenKey_SECCION2.txt')
 
 # Se crea una gráfica para comparar los filtros 
 fig3, ax3 = plt.subplots(2, 2, figsize=(12, 12))
@@ -190,20 +194,22 @@ ax3_big.set_title('Filtro orden 4', fontsize=18)
 ax3_big.set_xlabel('Frecuencia [Hz]', fontsize=15)
 ax3_big.set_ylabel('|H(jw)|² [dB]', fontsize=15)
 ax3_big.set_xscale('log')
-ax3_big.set_xlim(1, 1e5)
+ax3_big.set_xlim(1, 1e3)
 ax3_big.grid(True, which="both")
 ax3_big.plot(f,  20*np.log10((abs(h_1))) + 20*np.log10((abs(h_2))), label='Ideal')
 ax3_big.plot(f2, h_sim_1 + h_sim_2, label='Simulado')
+ax3_big.plot(freqPrueba, amplitudPrueba, label='Implementado')
 ax3_big.legend(loc="lower left", fontsize=15)
 plt.tight_layout()
 
-# Análisis de la atenuación del filtro simulado en las frecuencias de interés
-FS = 80
+## Análisis de la atenuación del filtro simulado en las frecuencias de interés
+#FS = 40
 # se calcula la atenuación en el punto mas cercano a la frecuencia de interés
-at_1 = h_sim_1[np.argmin(np.abs(f1-3000))] 
-at_2 = h_sim_2[np.argmin(np.abs(f2-3000))] 
-print("La atenuación del filtro simulado en {}Hz es de {:.2f}dB".format(FS, at_1+at_2))
+#at_1 = h_sim_1[np.argmin(np.abs(f1-3000))] 
+#at_2 = h_sim_2[np.argmin(np.abs(f2-3000))] 
+#print("La atenuación del filtro simulado en {}Hz es de {:.2f}dB".format(FS, at_1+at_2))
 
+#freqPrueba = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 80, 90, 100]
+#amplitudPrueba = [0, 0, 0, -0.0168, -0.340, -1.85, -3.69, -14.3, -18.75, -22.6, -24.7,-28, -29.45, -31.4, -34.3,-35.8, -38.75]
 
-
-
+#plt.plot(freqPrueba, amplitudPrueba, label='Implementado')
